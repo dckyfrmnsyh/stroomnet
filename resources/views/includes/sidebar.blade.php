77a14cc372e8d3ac4500c8b72d35e5a89a2ef91d
Dicky Firmansyah <45268672+Subrotoking@users.noreply.github.com>
2020-12-22 10:18:47 +0700

    update sidebar

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -3,17 +3,21 @@
     <nav class="navbar navbar-expand-sm navbar-default">
         <div id="main-menu" class="main-menu collapse navbar-collapse">
             <ul class="nav navbar-nav">
+            
                 <li class="">
                     <a href="{{ route('order') }}"><i class="menu-icon fa fa-shopping-cart"></i>Order </a>
                 </li>
+
                 @if(auth()->user()->hasRole('admin'))
                 <li class="">
                     <a href="{{route('order.all')}}"><i class="menu-icon fa fa-shopping-cart"></i>All Order </a>
                 </li>
                 @endif
+
                 <li class="">
                     <a href="{{ route('customer') }}"><i class="menu-icon fa fa-user"></i>Customer </a>
                 </li>
+
                 @if(auth()->user()->hasRole('admin'))
                 <li class="">
                     <a href="{{ route('user') }}"><i class="menu-icon fa fa-users"></i>User </a>
